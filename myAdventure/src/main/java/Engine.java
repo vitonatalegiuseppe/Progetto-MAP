0c7fbda4df14abf2d695499ec326d9c657866857
Iceberg01 <g.piemontese4@studenti.uniba.it>
2023-05-25 16:00:50 +0200

    classi aggiunte: command, engine, inventory

--- myAdventure/src/main/java/Engine.java
+++ myAdventure/src/main/java/Engine.java
@@ -2,11 +2,71 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+import di.uniba.map.b.adventure.type.CommandType;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Set;
 
 /**
  *
  * @author giuse
  */
 public class Engine {
-    
+   
+
+    private final GameDescription game;
+
+    private Parser parser;
+
+    public Engine(GameDescription game) {
+        this.game = game;
+        try {
+            this.game.init();
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
+        try {
+            Set<String> stopwords = Utils.loadFileListInSet(new File("./resources/stopwords"));
+            parser = new Parser(stopwords);
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+
+    public void execute() {
+        System.out.println("================================");
+        System.out.println("* Adventure v. 0.3 - 2021-2022 *");
+        System.out.println("================================");
+        System.out.println(game.getCurrentRoom().getName());
+        System.out.println();
+        System.out.println(game.getCurrentRoom().getDescription());
+        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            ParserOutput p = parser.parse(command, game.getCommands(), game.getCurrentRoom().getObjects(), game.getInventory());
+            if (p == null || p.getCommand() == null) {
+                System.out.println("Non capisco quello che mi vuoi dire.");
+            } else if (p.getCommand() != null && p.getCommand().getType() == CommandType.END) {
+                System.out.println("Addio!");
+                break;
+            } else {
+                game.nextMove(p, System.out);
+                System.out.println();
+            }
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Engine engine = new Engine(new FireHouseGame());
+        engine.execute();
+    }
+
 }
+
+    
+
--- myAdventure/src/main/java/Inventory.java
+++ myAdventure/src/main/java/Inventory.java
@@ -3,10 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author giuse
  */
-public class Inventory {
     
+    public class Inventory {
+
+    private List<Object> list = new ArrayList<>();
+
+    public List<Object> getList() {
+        return list;
+    }
+
+    public void setList(List<Object> list) {
+        this.list = list;
+    }
+
+    public void add(Object o) {
+        list.add(o);
+    }
+
+    public void remove(Object o) {
+        list.remove(o);
+    }
 }
+
+    
+
